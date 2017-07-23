f0d63a418729e6ae37cdfddbec17e439f47579bd
Richard Wilkes <wilkes@me.com>
2017-07-22 19:29:32 -0700

    Add ParseDuration

--- duration.go
+++ duration.go
@@ -2,9 +2,47 @@ package torrent
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/richardwilkes/errs"
 )
 
+// ParseDuration parses the duration string, as produced by FormatDuration().
+func ParseDuration(duration string) (time.Duration, error) {
+	parts := strings.Split(strings.TrimSpace(duration), ":")
+	if len(parts) != 3 {
+		return 0, errs.New("Invalid format")
+	}
+	hours, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return 0, errs.New("Invalid hour format")
+	}
+	minutes, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return 0, errs.New("Invalid minute format")
+	}
+	parts = strings.Split(parts[2], ".")
+	var seconds int
+	var millis int
+	switch len(parts) {
+	case 2:
+		if millis, err = strconv.Atoi(parts[1]); err != nil {
+			return 0, errs.New("Invalid millisecond format")
+		}
+		fallthrough
+	case 1:
+		if seconds, err = strconv.Atoi(parts[0]); err != nil {
+			return 0, errs.New("Invalid second format")
+		}
+	default:
+		return 0, errs.New("Invalid second format: too many decimal points")
+	}
+	return time.Duration(hours)*time.Hour + time.Duration(minutes)*time.Minute + time.Duration(seconds)*time.Second + time.Duration(millis)*time.Millisecond, nil
+}
+
+// FormatDuration formats the duration as either "0:00:00" or "0:00:00.000".
 func FormatDuration(duration time.Duration, includeMillis bool) string {
 	if duration < 0 {
 		duration = 0
